44d472332e615377208c88b5ce5b500b33afc2dc
Arturo Laurenzi <arturo.laurenzi@iit.it>
2016-10-10 17:52:06 +0200

    On with the implementation of RobotInterfaceROS!
    - added control mode definitions (as static const strings :( )
    - added implementation of control mode getter/setter
    - added control_mode_map to keep track of joints control mode (initialized at IDLE)
    - added utility function checkControlMode()
    - added check for joint existence during parsing
    - implemented sense_internal() and move_internal()

--- RobotInterfaceROS/include/RobotInterfaceROS/RobotInterfaceROS.h
+++ RobotInterfaceROS/include/RobotInterfaceROS/RobotInterfaceROS.h
@@ -12,6 +12,17 @@ namespace XBot {
         class RobotInterfaceROS : public RobotInterface {
 
         public:
+			
+			static const std::string control_mode_TORQUE;
+			static const std::string control_mode_IDLE;
+			static const std::string control_mode_POS;
+			static const std::string control_mode_POS_FFTORQUE;
+			
+
+			virtual bool getControlMode(std::map< std::string, std::string >& joint_control_mode_map); 
+			
+			virtual bool setControlMode(const std::map< std::string, std::string >& joint_control_mode_map);
+			virtual bool setControlMode(const std::string& robot_control_mode);
                 
         protected:
             
@@ -30,19 +41,41 @@ namespace XBot {
             
             ros::NodeHandle _nh;
             ros::Subscriber _joint_state_sub;
+			ros::Publisher _control_command_pub;
             
             sensor_msgs::JointState _joint_state_msg;
+			custom_messages::CustomCmnd _controller_msg;
+			
             bool _joint_state_received;
             
             std::string _joint_state_name;
             std::string _joint_service_name;
+			std::string _command_topic_name;
             
             std::map<std::string, int> _controller_ids;
             std::map<std::string, int> _joint_state_ids;
             
+			std::map<std::string, double> _jointstate_msg_map_position;
+			std::map<std::string, double> _jointstate_msg_map_velocity;
+			std::map<std::string, double> _jointstate_msg_map_effort;
+			
+			std::map<std::string, double> _command_msg_map_position;
+			std::map<std::string, double> _command_msg_map_velocity;
+			std::map<std::string, double> _command_msg_map_effort;
+			std::map<std::string, double> _command_msg_map_stiffness;
+			std::map<std::string, double> _command_msg_map_damping;
+			
+			std::map<std::string, std::string> _control_mode_map;
+			
+			bool checkControlMode(const std::string& control_mode) const;
             
             
                 
         };
+		
+		const std::string RobotInterfaceROS::control_mode_TORQUE = "TORQUE";
+		const std::string RobotInterfaceROS::control_mode_IDLE = "IDLE";
+		const std::string RobotInterfaceROS::control_mode_POS = "POSITION";
+		const std::string RobotInterfaceROS::control_mode_POS_FFTORQUE = "POS_FFTORQUE";
 }
 #endif
--- RobotInterfaceROS/src/RobotInterfaceROS.cpp
+++ RobotInterfaceROS/src/RobotInterfaceROS.cpp
@@ -5,7 +5,9 @@ XBot::RobotInterfaceROS::RobotInterfaceROS(const XBot::XBotCoreModel& XBotModel)
     RobotInterface(XBotModel),
     _joint_state_received(false)
 {
-
+	for( const std::string& joint_name : this->getEnabledJointNames() ){
+			_control_mode_map[joint_name] = control_mode_IDLE;
+	}
 }
 
 bool XBot::RobotInterfaceROS::init(const std::string& path_to_cfg)
@@ -20,10 +22,12 @@ bool XBot::RobotInterfaceROS::init(const std::string& path_to_cfg)
     // TBD check if they exist
     _joint_state_name = root_cfg["joint_state_name"].as<std::string>();
     _joint_service_name = root_cfg["joint_service_name"].as<std::string>();
-    
+    _command_topic_name = root_cfg["command_topic_name"].as<std::string>();
 
     bool joint_state_ok = parseJointStateIds();
     bool controller_ok = parseControllerIds();
+	
+	_control_command_pub = _nh.advertise<custom_messages::CustomCmnd>(_command_topic_name, 1);
 
     return joint_state_ok && controller_ok;
 }
@@ -41,8 +45,12 @@ bool XBot::RobotInterfaceROS::parseControllerIds()
     int cont = 0;
     for(const std::string& joint_name: res.joints) {
         
-        // TBD if(XBotModel.hasJoint(joint_name)
-        _controller_ids[joint_name] = res.ids[cont++];
+		if(this->hasJoint(joint_name)){
+			_controller_ids[joint_name] = res.ids[cont++];
+		}
+		else{ // TBD: what to do if controller response contains a non-existing (disabled) joint? Currently, nothing
+			std::cerr << "ERROR joint " << joint_name << " does not exist (or is disabled)!" << std::endl;
+		}
     }
     return true;
 }
@@ -68,8 +76,15 @@ bool XBot::RobotInterfaceROS::parseJointStateIds()
     int cont = 0;
     for(const std::string& joint_name: _joint_state_msg.name) {
         
-        // TBD if(XBotModel.has(joint_name)
+       if(this->hasJoint(joint_name)){
         _joint_state_ids[joint_name] = cont++;
+		_jointstate_msg_map_position[joint_name] = 0;
+		_jointstate_msg_map_velocity[joint_name] = 0;
+		_jointstate_msg_map_effort[joint_name] = 0;
+	   }
+	   else{ // TBD: what to do if joint state msg contains a non-existing (disabled) joint? Currently, nothing
+		   std::cerr << "ERROR joint " << joint_name << " does not exist (or is disabled)!" << std::endl;
+	   }
     }
     return true;
 }
@@ -84,12 +99,107 @@ void XBot::RobotInterfaceROS::jointStateCallback(sensor_msgs::JointState::ConstP
 bool XBot::RobotInterfaceROS::sense_internal()
 {
     ros::spinOnce();
+	// NOTE: should we check if we actually received any message?
+	// Right now NO (chains are updated even if msg is equal to last sense)
+	for(int i=0; i<_joint_state_msg.name.size(); i++){
+		
+		_jointstate_msg_map_position[_joint_state_msg.name[i]] = _joint_state_msg.position[i];
+		_jointstate_msg_map_velocity[_joint_state_msg.name[i]] = _joint_state_msg.velocity[i];
+		_jointstate_msg_map_effort[_joint_state_msg.name[i]] = _joint_state_msg.effort[i];
+	}
+	
+	this->setLinkPos(_jointstate_msg_map_position);
+	this->setLinkVel(_jointstate_msg_map_velocity);
+	this->setEffort(_jointstate_msg_map_effort);
+	
+	_joint_state_received = false;
     
 }
 
 
 bool XBot::RobotInterfaceROS::move_internal()
 {
+	// Get references from chains
+	this->getPosRef(_command_msg_map_position);
+	this->getVelRef(_command_msg_map_velocity);
+	this->getEffortRef(_command_msg_map_effort);
+	this->getStiffness(_command_msg_map_stiffness);
+	this->getDamping(_command_msg_map_damping);
+	
+	// Make sure we allocated enough memory (assumes consecutive indices) TBD support non-consecutive idx??
+	_controller_msg.position.reserve(_controller_ids.size());
+	_controller_msg.velocity.reserve(_controller_ids.size());
+	_controller_msg.effort.reserve(_controller_ids.size());
+	_controller_msg.onlineGain1.reserve(_controller_ids.size());
+	_controller_msg.onlineGain2.reserve(_controller_ids.size());
+	
+	// Fill the command message
+	for( const auto& jointname_id_pair : _controller_ids ) {
+	
+		const std::string& joint_name = jointname_id_pair.first;
+		int id = jointname_id_pair.second;
+		
+		_controller_msg.position[id] = _command_msg_map_position.at(joint_name);
+		_controller_msg.velocity[id] = _command_msg_map_velocity.at(joint_name);
+		_controller_msg.effort[id] = _command_msg_map_effort.at(joint_name);
+		_controller_msg.onlineGain1[id] = _command_msg_map_stiffness.at(joint_name);
+		_controller_msg.onlineGain2[id] = _command_msg_map_damping.at(joint_name);
+		
+	}
+	
+	// Send the command message
+	_control_command_pub.publish(_controller_msg);
+	
 
+}
+
+bool XBot::RobotInterfaceROS::getControlMode(std::map< std::string, std::string >& joint_control_mode_map)
+{
+	joint_control_mode_map = _control_mode_map;
+	return true;
+}
+
+bool XBot::RobotInterfaceROS::setControlMode(const std::string& robot_control_mode)
+{
+	if(!checkControlMode(robot_control_mode)){
+		std::cerr << "Requested control mode " << robot_control_mode << " does not exist in this controller!" << std::endl;
+		return false;
+	}
+	
+	for( auto& jointname_ctrlmode_pair : _control_mode_map ){
+			jointname_ctrlmode_pair.second = robot_control_mode;
+	}
+}
+
+bool XBot::RobotInterfaceROS::setControlMode(const std::map< std::string, std::string >& joint_control_mode_map)
+{
+	// Check for input consistency (joints exist and control modes are defined)
+	for( const auto& jointname_ctrlmode_pair : joint_control_mode_map ){
+
+		if(!this->hasJoint(jointname_ctrlmode_pair.first)){
+			std::cerr << "Joint " << jointname_ctrlmode_pair.first << " does not exist (or it's diabled)!" << std::endl;
+			return false;
+		}
+		if(!checkControlMode(jointname_ctrlmode_pair.second)){
+			std::cerr << "Requested control mode " << jointname_ctrlmode_pair.second << " does not exist in this controller!" << std::endl;
+			return false;
+		}
+		
+	}
+	
+	for( const auto& jointname_ctrlmode_pair : joint_control_mode_map ){
+			_control_mode_map.at(jointname_ctrlmode_pair.first) = jointname_ctrlmode_pair.second;
+	}
+	
+	return true;
+	
+}
+
+bool XBot::RobotInterfaceROS::checkControlMode(const std::string& control_mode) const
+{
+	return control_mode == control_mode_IDLE || 
+				control_mode == control_mode_POS ||
+				control_mode == control_mode_TORQUE ||
+				control_mode == control_mode_POS_FFTORQUE;
 }
 
