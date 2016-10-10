04e304bdcc50bffe1157e2463e3ad5b349f372eb
Arturo Laurenzi <arturo.laurenzi@iit.it>
2016-10-10 09:43:32 +0200

    Begun development of RobotInterfaceROS

---
+++ IXBotInterface/include/XBotInterface/RobotInterfaceROS.h
@@ -0,0 +1,22 @@
+#ifndef XBOT_ROBOTINTERFACE_ROS_H
+#define XBOT_ROBOTINTERFACE_ROS_H
+
+#include "XBotInterface/RobotInterface.h"
+#include <ros/ros.h>
+#include <sensor_msgs/JointState.h>
+#include "custom_messages/CustomCmd.h"
+#include "custom_services/GetJointNames.h"
+
+namespace XBot {
+	
+	class RobotInterfaceROS : public RobotInterface {
+		
+	public:
+		
+	protected:
+		
+	private:
+		
+	};
+}
+#endif
