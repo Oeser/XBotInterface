f42bccf8a2c89b9244a7b468161caaec4d814092
Arturo Laurenzi <arturo.laurenzi@iit.it>
2016-11-23 18:53:36 +0100

    Added a couple of simple examples

---
+++ examples/home_example.cpp
@@ -0,0 +1,49 @@
+#include <XBotInterface/RobotInterface.h>
+
+
+int main(int argc, char **argv){
+    
+    using XBot::Sync;
+ 
+    std::string path_to_config_file(argv[1]); // from command line
+
+    XBot::RobotInterface& robot = *XBot::RobotInterface::getRobot(path_to_config_file, argc, argv);
+    XBot::ModelInterface& model = *XBot::ModelInterface::getModel(path_to_config_file);
+    
+    Eigen::VectorXd qhome;
+    if(!robot.getRobotState("home", qhome)){
+        return -1;
+    }
+    
+    Eigen::VectorXd qinitial;
+    
+    robot.sense();
+    robot.getJointPosition(qinitial);
+
+    
+//     for(auto pair : qhome) std::cout << pair.first << ": " << pair.second << std::endl;
+    
+    double alpha = 0;
+    
+
+    
+    while(robot.isRunning()){
+        
+        alpha += 0.001;
+        alpha = alpha > 1 ? 1 : alpha;
+     
+        robot.sense();
+        robot.setPositionReference(qinitial+alpha*(qhome-qinitial));
+        
+        
+        robot.print();
+        
+        robot.move();
+        
+        usleep(10000);
+        
+    }
+    
+    return 0;
+    
+}
---
+++ examples/read_example.cpp
@@ -0,0 +1,30 @@
+#include <XBotInterface/RobotInterface.h>
+
+
+int main(int argc, char **argv){
+ 
+    std::string path_to_config_file(argv[1]); // from command line
+
+    XBot::RobotInterface& robot = *XBot::RobotInterface::getRobot(path_to_config_file, argc, argv);
+    XBot::ModelInterface& model = *XBot::ModelInterface::getModel(path_to_config_file);
+    
+    XBot::ForceTorqueMap ftmap = robot.getForceTorque();
+    XBot::ImuMap imumap = robot.getImu();
+    
+    while(robot.isRunning()){
+     
+        robot.sense();
+        
+        robot.print();
+        
+        for(auto& pair : ftmap) std::cout << *pair.second << std::endl;
+        for(auto& pair : imumap) std::cout << *pair.second << std::endl;
+        
+        
+        usleep(10000);
+        
+    }
+    
+    return 0;
+    
+}
