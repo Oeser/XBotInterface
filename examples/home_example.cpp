2ac60a438a57e0611228d310828861d8fe774016
Arturo Laurenzi <arturo.laurenzi@iit.it>
2017-01-19 11:14:24 +0100

    Now working

--- examples/home_example.cpp
+++ examples/home_example.cpp
@@ -2,52 +2,52 @@
 #include <XBotInterface/Logger.hpp>
 
 int main(int argc, char **argv){
-    
+
     using XBot::Sync;
-    
-    XBot::Logger logger("home_logger");
- 
+
+    auto& logger = *XBot::ConsoleLogger::getLogger("home_logger");
+
     std::string path_to_config_file(argv[1]); // from command line
 
     XBot::RobotInterface& robot = *XBot::RobotInterface::getRobot(path_to_config_file);
     XBot::ModelInterface& model = *XBot::ModelInterface::getModel(path_to_config_file);
-    
+
     Eigen::VectorXd qhome;
     if(!robot.getRobotState("home", qhome)){
         return -1;
     }
-    
+
     logger.info().log() << "q homing : " << qhome << logger.endl();
-    
+
     Eigen::VectorXd qinitial;
-    
+
     robot.sense();
     robot.getJointPosition(qinitial);
 
-    
-//     for(auto pair : qhome) std::cout << pair.first << ": " << pair.second << std::endl;
-    
-    double alpha = 0;
-    
 
-    
+//     for(auto pair : qhome) std::cout << pair.first << ": " << pair.second << std::endl;
+
+    double alpha = 0;
+
+
+
     while(robot.isRunning()){
-        
+
         alpha += 0.001;
         alpha = alpha > 1 ? 1 : alpha;
-     
+
         robot.sense();
         robot.setPositionReference(qinitial+alpha*(qhome-qinitial));
-        
-        
+
+
         logger.info().log() << "q ref : " << qinitial+alpha*(qhome-qinitial) << logger.endl();
-        
+
         robot.move();
-        
+
         usleep(10000);
-        
+
     }
-    
+
     return 0;
-    
+
 }
--- examples/log_example.cpp
+++ examples/log_example.cpp
@@ -8,29 +8,31 @@
 
 int main(int argc, char **argv){
 
-    XBot::SPDMatLogger logger("m_log", "/tmp/rt_test.m");
+    auto& file_logger = *XBot::FileLogger::getLogger("file_logger", "/tmp/test_file_logger.txt");
+    auto& console_logger = *XBot::ConsoleLogger::getLogger();
+    auto& mfile_logger = *XBot::MatLogger::getLogger("mfile_logger", "/tmp/test_mfile_logger.txt");
     Eigen::VectorXd q;
     Eigen::MatrixXd A;
-    
+
 
     q.setConstant(5,0.123456789);
     A.setConstant(6,6,0.123456789);
 
     int i = 0;
     while(i < LOG_ITERATION) {
-        
-//         logger.info().log() << q.transpose() << 12 << "test1 " << __func__  << q.transpose() << "test2 " << logger.endl();
-//         logger.warning().log() << "warn1 " << __func__  << q.transpose() << " warn2 " << logger.endl();
-//         logger.error().log() << "error  " << __func__  << q.transpose() << " --- " << logger.endl();
-        
-        logger.add("q_v", q);
-        logger.add("A_v", A);
-        logger.add("scalar", double(i)/1.2345);
-        
+
+        file_logger.info().log() << q.transpose() << 12 << "test1 " << __func__  << q.transpose() << "test2 " << file_logger.endl();
+        console_logger.warning().log() << "warn1 " << __func__  << q.transpose() << " warn2 " << console_logger.endl();
+        console_logger.error().log() << "error  " << __func__  << q.transpose() << " --- " << console_logger.endl();
+
+        mfile_logger.add("q_v", q);
+        mfile_logger.add("A_v", A);
+        mfile_logger.add("scalar", double(i)/1.2345);
+
         usleep(LOG_PERIOD_MICRO);
         i++;
     }
-    
+
     return 0;
 
 }
