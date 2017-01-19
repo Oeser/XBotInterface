3bd66349d28d801663578096a823e9339eaade89
Luca Muratore <luca.muratore@iit.it>
2017-01-19 10:46:04 +0100

    Logger with factories, not compiling

--- examples/log_example.cpp
+++ examples/log_example.cpp
@@ -8,17 +8,24 @@
 
 int main(int argc, char **argv){
 
-    XBot::Logger logger("test_logger");
+    XBot::SPDMatLogger logger("m_log", "/tmp/rt_test.m");
     Eigen::VectorXd q;
+    Eigen::MatrixXd A;
+    
 
-    q.setConstant(5,5);
+    q.setConstant(5,0.123456789);
+    A.setConstant(6,6,0.123456789);
 
     int i = 0;
     while(i < LOG_ITERATION) {
         
-        logger.info().log() << q.transpose() << 12 << "test1 " << __func__  << q.transpose() << "test2 " << logger.endl();
-        logger.warning().log() << "warn1 " << __func__  << q.transpose() << " warn2 " << logger.endl();
-        logger.error().log() << "error  " << __func__  << q.transpose() << " --- " << logger.endl();
+//         logger.info().log() << q.transpose() << 12 << "test1 " << __func__  << q.transpose() << "test2 " << logger.endl();
+//         logger.warning().log() << "warn1 " << __func__  << q.transpose() << " warn2 " << logger.endl();
+//         logger.error().log() << "error  " << __func__  << q.transpose() << " --- " << logger.endl();
+        
+        logger.add("q_v", q);
+        logger.add("A_v", A);
+        logger.add("scalar", double(i)/1.2345);
         
         usleep(LOG_PERIOD_MICRO);
         i++;
