4cbb26aa30b92b99b67ba18ae765d8c3c6e62ecb
Arturo Laurenzi <arturo.laurenzi@iit.it>
2017-01-19 11:49:01 +0100

    Fixed example + fixed logging pattern (was set globally to %v)

--- examples/log_example.cpp
+++ examples/log_example.cpp
@@ -8,9 +8,11 @@
 
 int main(int argc, char **argv){
 
-    auto& file_logger = *XBot::FileLogger::getLogger("file_logger", "/tmp/test_file_logger.txt");
+    auto& file_logger = *XBot::FileLogger::getLogger("file_logger", "/tmp/test_file_logger");
     auto& console_logger = *XBot::ConsoleLogger::getLogger();
-    auto& mfile_logger = *XBot::MatLogger::getLogger("mfile_logger", "/tmp/test_mfile_logger.txt");
+
+    auto& mfile_logger = *XBot::MatLogger::getLogger("mfile_logger", "/tmp/test_mfile_logger.m");
+
     Eigen::VectorXd q;
     Eigen::MatrixXd A;
 
@@ -21,9 +23,9 @@ int main(int argc, char **argv){
     int i = 0;
     while(i < LOG_ITERATION) {
 
-        file_logger.info() << q.transpose() << 12 << "test1 " << __func__  << q.transpose() << "test2 " << file_logger.endl();
-        console_logger.warning() << "warn1 " << __func__  << q.transpose() << " warn2 " << console_logger.endl();
-        console_logger.error() << "error  " << __func__  << q.transpose() << " --- " << console_logger.endl();
+        file_logger.info() << "Vector q is :" << q.transpose() << file_logger.endl();
+        console_logger.warning() << "Vector q is :" << q.transpose() << console_logger.endl();
+        console_logger.error() << "Vector q is :" << q.transpose() << console_logger.endl();
 
         mfile_logger.add("q_v", q);
         mfile_logger.add("A_v", A);
