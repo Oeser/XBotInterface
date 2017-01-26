dc6b054a3ef1425d05d710a666e34e23473f9226
Arturo Laurenzi <arturo.laurenzi@iit.it>
2017-01-26 18:23:26 +0100

    Brand now logger, performance is unbelievable

--- examples/log_example.cpp
+++ examples/log_example.cpp
@@ -3,7 +3,7 @@
 
 #include <unistd.h>
 
-#define LOG_ITERATION 40e3
+#define LOG_ITERATION 4e3
 #define LOG_PERIOD_MICRO 1e4
 
 int main(int argc, char **argv){
@@ -11,11 +11,13 @@ int main(int argc, char **argv){
     auto& file_logger = *XBot::FileLogger::getLogger("file_logger", "/tmp/test_file_logger");
     auto& console_logger = *XBot::ConsoleLogger::getLogger();
 
-    auto& mfile_logger = *XBot::MatLogger::getLogger("mfile_logger", "/tmp/test_mfile_logger");
+    auto& mfile_logger = *XBot::MatLogger::getLogger("/tmp/test_mfile_logger.mat");
 
     Eigen::VectorXd q;
     Eigen::MatrixXd A;
 
+
+    mfile_logger.createVectorVariable("q_v", 5, 1, 3000);
 
     q.setConstant(5,0.123456789);
     A.setConstant(6,6,0.123456789);
@@ -28,8 +30,8 @@ int main(int argc, char **argv){
         console_logger.error() << "Vector q is :" << q.transpose() << console_logger.endl();
 
         mfile_logger.add("q_v", q*std::sin(i/100.0));
-        mfile_logger.add("A_v", A);
-        mfile_logger.add("scalar", double(i)/1.2345);
+//         mfile_logger.add("A_v", A);
+//         mfile_logger.add("scalar", double(i)/1.2345);
 
 //         usleep(LOG_PERIOD_MICRO);
         i++;
