831f1bc79c8f5c6372b6cb9e2412741fbb7d1314
Arturo Laurenzi <arturo.laurenzi@iit.it>
2017-02-08 21:31:29 +0100

    RobotInterface implementation can now be selected from  getRobot

--- src/RobotInterface.cpp
+++ src/RobotInterface.cpp
@@ -20,7 +20,7 @@
 #include <XBotInterface/RobotInterface.h>
 #include <ros/ros.h> //TBD: remove include
 
-// NOTE Static members need to be defined in the cpp 
+// NOTE Static members need to be defined in the cpp
 std::string XBot::RobotInterface::_framework;
 std::string XBot::RobotInterface::_subclass_name;
 std::string XBot::RobotInterface::_path_to_shared_lib;
@@ -34,7 +34,7 @@ XBot::RobotInterface::RobotInterface()
 {
 }
 
-bool XBot::RobotInterface::parseYAML(const std::string &path_to_cfg)
+bool XBot::RobotInterface::parseYAML(const std::string &path_to_cfg, const std::string& framework)
 {
     std::ifstream fin(path_to_cfg);
     if (fin.fail()) {
@@ -47,7 +47,7 @@ bool XBot::RobotInterface::parseYAML(const std::string &path_to_cfg)
     YAML::Node x_bot_interface;
     // XBotInterface info
     if(root_cfg["RobotInterface"]) {
-        x_bot_interface = root_cfg["RobotInterface"]; 
+        x_bot_interface = root_cfg["RobotInterface"];
     }
     else {
         std::cerr << "ERROR in " << __func__ << " : YAML file  " << path_to_cfg << "  does not contain RobotInterface mandatory node!!" << std::endl;
@@ -61,20 +61,24 @@ bool XBot::RobotInterface::parseYAML(const std::string &path_to_cfg)
         std::cerr << "ERROR in " << __func__ << " : RobotInterface node of  " << path_to_cfg << "  does not contain framework_name mandatory node!!" << std::endl;
         return false;
     }
-    
+
+    if( framework != "" ){
+        _framework = framework;
+    }
+
     // subclass forced
     _subclass_name = std::string("RobotInterface") + _framework;
     // check the path to shared lib
     if(root_cfg[_subclass_name]["path_to_shared_lib"]) {
-        computeAbsolutePath(root_cfg[_subclass_name]["path_to_shared_lib"].as<std::string>(), 
+        computeAbsolutePath(root_cfg[_subclass_name]["path_to_shared_lib"].as<std::string>(),
                             LIB_MIDDLE_PATH,
-                            _path_to_shared_lib); 
+                            _path_to_shared_lib);
     }
     else {
         std::cerr << "ERROR in " << __func__ << " : YAML file  " << path_to_cfg << "  does not contain " << _subclass_name << " mandatory node!!" << std::endl;
         return false;
     }
-    
+
     if(root_cfg[_subclass_name]["subclass_factory_name"]) {
         _subclass_factory_name = root_cfg[_subclass_name]["subclass_factory_name"].as<std::string>();
     }
@@ -87,22 +91,25 @@ bool XBot::RobotInterface::parseYAML(const std::string &path_to_cfg)
 }
 
 
-XBot::RobotInterface::Ptr XBot::RobotInterface::getRobot(const std::string &path_to_cfg, AnyMapConstPtr any_map)
+XBot::RobotInterface::Ptr XBot::RobotInterface::getRobot(const std::string &path_to_cfg,
+                                        AnyMapConstPtr any_map,
+                                        const std::string& framework)
 {
     // NOTE singleton
     if (_instance_ptr) {
         return _instance_ptr;
     }
     // parsing YAML
-    if (!parseYAML(path_to_cfg)) {
+    if (!parseYAML(path_to_cfg, framework)) {
         std::cerr << "ERROR in " << __func__ << " : could not parse the YAML " << path_to_cfg << " . See error above!!" << std::endl;
         return _instance_ptr;
     }
 
+
     // loading the requested model interface internal to the robot
     _model = XBot::ModelInterface::getModel(path_to_cfg);
-    
-    
+
+
     // loading the requested robot interface
     _robot_interface_factory.open( _path_to_shared_lib.c_str(),
                                                                                 _subclass_factory_name.c_str());
@@ -112,13 +119,13 @@ XBot::RobotInterface::Ptr XBot::RobotInterface::getRobot(const std::string &path
                _robot_interface_factory.getLastNativeError().c_str());
     }
     // open and init robot interface
-    _robot_interface_instance.open(_robot_interface_factory); 
+    _robot_interface_instance.open(_robot_interface_factory);
     _robot_interface_instance->init(path_to_cfg, any_map);
     // static instance of the robot interface
     _instance_ptr = std::shared_ptr<RobotInterface>(&_robot_interface_instance.getContent(), [](RobotInterface* ptr){return;});
-    
-    
-    
+
+
+
     return _instance_ptr;
 
 }
@@ -152,21 +159,21 @@ bool XBot::RobotInterface::move()
 bool XBot::RobotInterface::init_internal(const std::string& path_to_cfg, AnyMapConstPtr any_map)
 {
     // Fill _robot_chain_map with shallow copies of chains in _chain_map
-    
+
     _robot_chain_map.clear();
-    
+
     for( const auto& c : _chain_map){
-        
+
         RobotChain::Ptr robot_chain(new RobotChain());
         robot_chain->shallowCopy(*c.second);
-        
+
         _robot_chain_map[c.first] = robot_chain;
-        
+
     }
-    
+
     // Call virtual init_robot
     bool success = init_robot(path_to_cfg, any_map);
-    
+
     _ordered_chain_names.clear();
     for( const std::string& s : model().getModelOrderedChainName() ){
             if( s == "virtual_chain" ){}
@@ -174,7 +181,7 @@ bool XBot::RobotInterface::init_internal(const std::string& path_to_cfg, AnyMapC
                 _ordered_chain_names.push_back(s);
             }
     }
-    
+
     // Since fixed controlled joints do not appear in the model, some chains may
     // be missing. So we add them!
     for( const auto& pair : _chain_map ){
@@ -183,7 +190,7 @@ bool XBot::RobotInterface::init_internal(const std::string& path_to_cfg, AnyMapC
             _ordered_chain_names.push_back(pair.first);
         }
     }
-    
+
     return success;
 }
 
@@ -260,14 +267,14 @@ void XBot::RobotInterface::getControlMode(std::map< std::string, XBot::ControlMo
 bool XBot::RobotInterface::setControlMode(const std::map< int, XBot::ControlMode >& control_mode)
 {
     bool success = true;
-    
+
     for( const auto& pair: control_mode ){
-        
-        
+
+
         if( hasJoint(pair.first) ){
-            
+
             bool set_internal_success = set_control_mode_internal(pair.first, pair.second);
-            
+
             if(set_internal_success){
                 getJointByIdInternal(pair.first)->setControlMode(pair.second);
                 std::cout << "INFO: Joint " << getJointByIdInternal(pair.first)->getJointName() <<
@@ -277,53 +284,53 @@ bool XBot::RobotInterface::setControlMode(const std::map< int, XBot::ControlMode
                 std::cout << "WARNING: Joint " << getJointByIdInternal(pair.first)->getJointName() <<
                              " - with id : " << pair.first << " - CANNOT change control mode to " << pair.second.getName() << std::endl;
             }
-            
+
             success = success && set_internal_success;
         }
-        
+
     }
-    
+
     return success;
 }
 
 bool XBot::RobotInterface::setControlMode(const std::map< std::string, XBot::ControlMode >& control_mode)
 {
     bool success = true;
-    
+
     for( const auto& pair: control_mode ){
 
         if( hasJoint(pair.first) ){
-            
+
             bool set_internal_success = set_control_mode_internal(getJointByNameInternal(pair.first)->getJointId(), pair.second);
-            
+
             if(set_internal_success){
                 getJointByNameInternal(pair.first)->setControlMode(pair.second);
                 std::cout << "INFO: Joint " << pair.first << " - with id : "
-                          << getJointByNameInternal(pair.first)->getJointId() 
+                          << getJointByNameInternal(pair.first)->getJointId()
                           << " - control mode changed to " << pair.second.getName() << std::endl;
             }
             else {
-                std::cout << "WARNING: Joint " << pair.first  << " - with id : " 
-                          << getJointByNameInternal(pair.first)->getJointId() << " - CANNOT change control mode to " 
+                std::cout << "WARNING: Joint " << pair.first  << " - with id : "
+                          << getJointByNameInternal(pair.first)->getJointId() << " - CANNOT change control mode to "
                           << pair.second.getName() << std::endl;
             }
-            
+
             success = set_internal_success && success;
         }
-        
+
     }
-    
+
     return success;
 }
 
 bool XBot::RobotInterface::setControlMode(const XBot::ControlMode& control_mode)
 {
     bool success = true;
-    
+
     for( int i = 0; i < getJointNum(); i++ ){
-        
+
         bool set_internal_success = success = set_control_mode_internal(_joint_vector[i]->getJointId(), control_mode);
-        
+
         if(set_internal_success){
             _joint_vector[i]->setControlMode(control_mode);
             std::cout << "INFO: Joint " << _joint_vector[i]->getJointName() << " - with id : "
@@ -331,14 +338,14 @@ bool XBot::RobotInterface::setControlMode(const XBot::ControlMode& control_mode)
                       << " - control mode changed to " << control_mode.getName() << std::endl;
         }
         else {
-            std::cout << "WARNING: Joint " << _joint_vector[i]->getJointName()  << " - with id : " 
-                      << _joint_vector[i]->getJointId() << " - CANNOT change control mode to " 
+            std::cout << "WARNING: Joint " << _joint_vector[i]->getJointName()  << " - with id : "
+                      << _joint_vector[i]->getJointId() << " - CANNOT change control mode to "
                       << control_mode.getName() << std::endl;
         }
-        
+
         success = set_internal_success && success;
     }
-    
+
     return success;
 }
 
@@ -347,18 +354,18 @@ bool XBot::RobotInterface::setControlMode(const XBot::ControlMode& control_mode)
 bool XBot::RobotInterface::setControlMode(const std::string& chain_name, const XBot::ControlMode& control_mode)
 {
     auto it = _chain_map.find(chain_name);
-    
+
     if( it == _chain_map.end() ){
         std::cerr << "ERROR in " << __func__ << "! Chain " << chain_name << " is NOT defined!" << std::endl;
         return false;
     }
-    
+
     bool success = true;
-    
+
     for( int i = 0; i < it->second->getJointNum(); i++){
-        
+
         bool set_internal_success = set_control_mode_internal(it->second->getJoint(i)->getJointId(), control_mode);
-        
+
         if( set_internal_success ){
             it->second->getJointInternal(i)->setControlMode(control_mode);
             std::cout << "INFO: Joint " << (it->second->getJoint(i)->getJointName()) << " - with id : "
@@ -366,14 +373,14 @@ bool XBot::RobotInterface::setControlMode(const std::string& chain_name, const X
                       << " - control mode changed to " << control_mode.getName() << std::endl;
         }
         else {
-            std::cout << "WARNING: Joint " << (it->second->getJoint(i)->getJointName())  << " - with id : " 
-                      << (it->second->getJoint(i)->getJointId()) << " - CANNOT change control mode to " 
+            std::cout << "WARNING: Joint " << (it->second->getJoint(i)->getJointName())  << " - with id : "
+                      << (it->second->getJoint(i)->getJointId()) << " - CANNOT change control mode to "
                       << control_mode.getName() << std::endl;
         }
-        
+
         success = success && set_internal_success;
     }
-    
+
     return success;
 }
 
