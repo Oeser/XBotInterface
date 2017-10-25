6c10fc4657381a77a42319657f328d1d842bd596
Arturo Laurenzi <arturo.laurenzi@iit.it>
2017-10-25 17:24:46 +0200

    Cleaned up code

--- examples/log_example.cpp
+++ examples/log_example.cpp
@@ -8,13 +8,16 @@ int main(int argc, char **argv){
 
     using namespace XBot::experimental;
     
+    Logger::SetVerbosityLevel(Logger::Severity::MID);
+    
     Logger::info() << "babcabc" << Logger::endl();
     
     Logger::error() << "cabcabc" << Logger::endl();
     
-    Logger::warning() << "cabcabc" << Logger::endl();
+    Logger::warning(Logger::Severity::LOW) << "cabcabc" << Logger::endl(); // not printed
     
     Logger::success() << "cabcabc" << Logger::endl();
+    
     
 
 }
